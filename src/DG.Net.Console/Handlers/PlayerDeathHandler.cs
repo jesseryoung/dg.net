1d19afe3c17dfd6be1e9930308afd0e7545d6027
Jesse Young <jesse@monkeybased.com>
2022-07-25 01:24:41 +0000

    Adding and fixing more rules

--- src/DG.Net.Console/Handlers/PlayerDeathHandler.cs
+++ src/DG.Net.Console/Handlers/PlayerDeathHandler.cs
@@ -27,7 +27,7 @@ public class PlayerDeathHandler : IPluginEventHandler<PlayerDeathEventMessage>
         {
             int totalDrinks = 0;
             var drinkMessages = new List<string>();
-            foreach(var rule in this.rules)
+            foreach (var rule in this.rules)
             {
                 if (rule.ShouldDrink(message))
                 {
@@ -49,14 +49,14 @@ public class PlayerDeathHandler : IPluginEventHandler<PlayerDeathEventMessage>
 
 public static class PlayerExtensions
 {
-    public static bool IsPlayerDrinking(this string? playerName) 
+    public static bool IsPlayerDrinking(this string? playerName)
     {
         if (playerName is null)
         {
             return false;
         }
 
-        return Regex.Match(playerName, @"[\[\{][SD]C?G[\]\}]", RegexOptions.IgnoreCase).Success;       
+        return Regex.Match(playerName, @"[\[\{][SD]C?G[\]\}]", RegexOptions.IgnoreCase).Success;
     }
 }
 
@@ -90,4 +90,164 @@ public class AssisterDrinkingRule : IGameRule
     public string BuildMessage(PlayerDeathEventMessage message) => "You were kill assisted by [DG]";
     public int CalculateDrinks(PlayerDeathEventMessage message) => 1;
     public bool ShouldDrink(PlayerDeathEventMessage message) => message.assister_name.IsPlayerDrinking() && message.attacker_client_id != message.userid_client_id;
+}
+
+public class SynergyDrinkingRule : IGameRule
+{
+    public string BuildMessage(PlayerDeathEventMessage message) => "Drinker synergy bonus";
+    public int CalculateDrinks(PlayerDeathEventMessage message) => 1;
+    public bool ShouldDrink(PlayerDeathEventMessage message) => message.attacker_name.IsPlayerDrinking() && message.assister_name.IsPlayerDrinking() && message.attacker_client_id != message.userid_client_id;
+}
+
+public class TauntDrinkingRule : IGameRule
+{
+    public string BuildMessage(PlayerDeathEventMessage message) => "Killed by a taunt kill";
+    public int CalculateDrinks(PlayerDeathEventMessage message) => 6;
+    public bool ShouldDrink(PlayerDeathEventMessage message) => message.attacker_name.IsPlayerDrinking() && message.weapon?.Contains("taunt") == true;
+}
+
+public class TrainDrinkingRule : IGameRule
+{
+    const byte DMG_VEHICLE = 1 << 4;
+    public string BuildMessage(PlayerDeathEventMessage message) => "You got run over by a train";
+    public int CalculateDrinks(PlayerDeathEventMessage message) => 6;
+    public bool ShouldDrink(PlayerDeathEventMessage message) => message.damagebits.HasValue && (message.damagebits.Value & DMG_VEHICLE) == DMG_VEHICLE;
+}
+
+/*
+#define TF_DEATHFLAG_KILLERDOMINATION   (1 << 0)
+#define TF_DEATHFLAG_ASSISTERDOMINATION (1 << 1)
+#define TF_DEATHFLAG_KILLERREVENGE      (1 << 2)
+#define TF_DEATHFLAG_ASSISTERREVENGE    (1 << 3)
+#define TF_DEATHFLAG_FIRSTBLOOD         (1 << 4)
+#define TF_DEATHFLAG_DEADRINGER         (1 << 5)
+*/
+
+public class AttackerDominatedDrinkingRule : IGameRule
+{
+    const byte TF_DEATHFLAG_KILLERDOMINATION = (1 << 0);
+    const byte TF_DEATHFLAG_KILLERREVENGE = (1 << 2);
+    const byte DMG_VEHICLE = 1 << 4;
+    public string BuildMessage(PlayerDeathEventMessage message) => "Attacker dominated/revenged you";
+    public int CalculateDrinks(PlayerDeathEventMessage message) => 2;
+    public bool ShouldDrink(PlayerDeathEventMessage message)
+    {
+        if (message.death_flags.HasValue && message.attacker_name.IsPlayerDrinking())
+        {
+            var dominated = (message.death_flags.Value & TF_DEATHFLAG_KILLERDOMINATION) == TF_DEATHFLAG_KILLERDOMINATION;
+            var revenged = (message.death_flags.Value & TF_DEATHFLAG_KILLERREVENGE) == TF_DEATHFLAG_KILLERREVENGE;
+            return dominated || revenged;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class AssisterDominatedDrinkingRule : IGameRule
+{
+    const byte TF_DEATHFLAG_ASSISTERDOMINATION = 1 << 1;
+    const byte TF_DEATHFLAG_ASSISTERREVENGE = 1 << 3;
+    const byte DMG_VEHICLE = 1 << 4;
+    public string BuildMessage(PlayerDeathEventMessage message) => "Assister dominated/revenged you";
+    public int CalculateDrinks(PlayerDeathEventMessage message) => 2;
+    public bool ShouldDrink(PlayerDeathEventMessage message)
+    {
+        if (message.death_flags.HasValue && message.assister_name.IsPlayerDrinking())
+        {
+            var dominated = (message.death_flags.Value & TF_DEATHFLAG_ASSISTERDOMINATION) == TF_DEATHFLAG_ASSISTERDOMINATION;
+            var revenged = (message.death_flags.Value & TF_DEATHFLAG_ASSISTERREVENGE) == TF_DEATHFLAG_ASSISTERREVENGE;
+            return dominated || revenged;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class SpecialWeaponDrinkingRule : IGameRule
+{
+    private static readonly Dictionary<string, int> drinkMap = new()
+    {
+		{ "amputator", 2 },
+		{ "armageddon", 6},
+		{ "axtinguisher", 2 },
+		{ "back_scratcher", 2 },
+		{ "ball", 6 },
+		{ "bat_wood", 2 },
+		{ "bat", 2 },
+		{ "battleaxe", 2 },
+		{ "battleneedle", 2 },
+		{ "bleed_kill", 3 },
+		{ "bonesaw", 2 },
+		{ "boston_basher", 2 },
+		{ "bottle", 2 },
+		{ "bushwacka", 2 },
+		{ "candy_cane", 2 },
+		{ "claidheamohmor", 2 },
+		{ "club", 2 },
+		{ "deflect_arrow", 10 },
+		{ "deflect_rocket", 4 },
+		{ "demokatana", 2 },
+		{ "demoshield", 3 },
+		{ "disciplinary_action", 2 },
+		{ "eternal_reward", 2 },
+		{ "eviction_notice", 2 },
+		{ "fireaxe", 2 },
+		{ "fists", 2 },
+		{ "fryingpan", 2 },
+		{ "gloves_running_urgently", 2 },
+		{ "gloves", 2 },
+		{ "headtaker", 2 },
+		{ "holiday_punch", 2 },
+		{ "holy_mackerel", 4 },
+		{ "holymackerel", 5 },
+		{ "hot_hand", 5},
+		{ "knife", 2 },
+		{ "lava_axe", 2 },
+		{ "lava_bat", 2 },
+		{ "mailbox", 2 },
+		{ "market_gardner", 3 },
+		{ "nessieclub", 5},
+		{ "nonnonviolent_protest", 3 },
+		{ "paintrain", 2 },
+		{ "persian_persuader", 2 },
+		{ "pickaxe", 2 },
+		{ "powerjack", 3 },
+		{ "robot_arm_combo_kill", 2 },
+		{ "robot_arm_kill", 2 },
+		{ "robot_arm", 2 },
+		{ "sandman", 2 },
+		{ "shahanshah", 2 },
+		{ "sharp_dresser", 2 },
+		{ "shovel", 2 },
+		{ "sledgehammer", 2 },
+		{ "solemn_vow", 2 },
+		{ "southern_comfort_kill", 2 },
+		{ "southern_hospitality", 2 },
+		{ "splendid_screen", 3 },
+		{ "spy_cicle", 2 },
+		{ "steel_fists", 2 },
+		{ "sword", 2 },
+		{ "the_maul", 2 },
+		{ "thirddegree", 2 },
+		{ "tribalkukri", 2 },
+		{ "ubersaw", 2 },
+		{ "ullapool_caber_explosion", 2 },
+		{ "ullapool_caber", 3 },
+		{ "warfan", 20 },
+		{ "warrior_spirit", 2 },
+		{ "world", 1 },
+		{ "wrap_assassin", 10 },
+		{ "wrench_jag", 2 },
+		{ "wrench", 2 },
+	};
+
+    public string BuildMessage(PlayerDeathEventMessage message) => "Killed by a special weapon";
+
+    public int CalculateDrinks(PlayerDeathEventMessage message) => drinkMap[message.weapon!];
+
+    public bool ShouldDrink(PlayerDeathEventMessage message) => message.attacker_name.IsPlayerDrinking() && message.weapon != null && drinkMap.ContainsKey(message.weapon);
 }
